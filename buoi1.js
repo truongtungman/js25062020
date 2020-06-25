a5971ec3b8503486337179b5a7a2be39af77e28c
manman1993 <truongtungman@gmail.com>
2020-06-25 21:34:59 +0700

    buoi1

--- buoi1.js
+++ buoi1.js
@@ -7,14 +7,44 @@
 // console.log(b)
 // var a = null
 // console.log(a)
-// undenfinedgit
+// 3.Undenfined
 // TH1 khai báo 1 biến không gán giá trị
 // Th2 Truy vấn tới key không tồn tại
+// TH3 Function không return tra về undenfine
 // const teo = {
 //     name: 'Nguyen van teo',
 //     age: 10
 // }
 // console.log(teo['name'])
-// ARRAY
-const arrayNames = ['teo', 'Ti', 'Tun']
-console.log(arrayNames[0])
+// 4. ARRAY
+// const arrayNames = ['teo', 'Ti', 'Tun']
+// arrayNames[0] = "Tuan"
+// console.log(arrayNames[0])
+// 5. Toan tu
+// + - * / %
+// var a = 5
+// var b = 6
+// var ketqua = a++ - --b + --a + b-- + ++a - b++ + b-- - b++
+// // 5- --b + --a +b-- + ++a -b++ + b-- -b++  a= 6,b=6
+// // 5- 5  + --a +b-- + ++a -b++ + b-- -b++  a= 6,b=5
+// // 5- 5  + --a +b-- + ++a -b++ + b-- -b++  a= 6,b=5
+
+// // 5 -5 +5 + 5 + 6 -4 + 5 -4  a= 6 b= 5
+// console.log(ketqua)
+// 6. FUNCTION
+// Phạm vi truy cập (access modifier)
+// function showName(name) {
+//     console.log(name)
+//     return name
+// }
+// console.log(showName("Phat"))
+// 7 : Object method
+const teo = {
+    name: "Nguyen Van Teo",
+    age: 10,
+    info: function () {
+        console.log("Name" + this.name)
+        console.log("Age" + this.age)
+    }
+}
+teo.info()
