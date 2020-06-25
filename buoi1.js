2633fd658b94c582b19602cebd90af0f0d9e80a6
manman1993 <truongtungman@gmail.com>
2020-06-25 20:57:35 +0700

    array

--- buoi1.js
+++ buoi1.js
@@ -5,5 +5,16 @@
 // b = 12
 // console.log(a)
 // console.log(b)
-var a = null
-console.log(a)
+// var a = null
+// console.log(a)
+// undenfinedgit
+// TH1 khai báo 1 biến không gán giá trị
+// Th2 Truy vấn tới key không tồn tại
+// const teo = {
+//     name: 'Nguyen van teo',
+//     age: 10
+// }
+// console.log(teo['name'])
+// ARRAY
+const arrayNames = ['teo', 'Ti', 'Tun']
+console.log(arrayNames[0])
